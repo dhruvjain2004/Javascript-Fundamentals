7b335ebd494c003070063dd2fdf41e6e2d38953d
Dhruv Jain <114809832+dhruvjain2004@users.noreply.github.com>
2025-02-04 17:37:01 +0530

    Create variable.js

---
+++ core js/variable.js
@@ -0,0 +1,10 @@
+fullname = "dhruvjain"
+let age = 20;
+roll_num = 18;
+console.log(fullname);
+console.log(age);
+console.log(roll_num);
+let age1 = 26;
+console.log(age1);
+const b = 89;
+console.log(b);
