875c874ea834aab7395c92bde29f084d6bde0bfe
Dhruv Jain <114809832+dhruvjain2004@users.noreply.github.com>
2025-02-04 17:45:52 +0530

    Update and rename Core Javascript to String.js

---
+++ String.js
@@ -0,0 +1,17 @@
+//strings in js
+let str = "dhruvjain";
+let str1 = "laxmijain";
+str.length;
+str[0];
+str1[0];
+str.touppercase()
+str.tolowercase()
+str.trim()
+str.slice(start,end?)
+str.concat((str1)
+str.replace(searchval,newval)
+str.charAt(idx)
+//q5
+let fname = prompt("enter ur full name without spaces :");
+let username = "@"+fname+fname.length;
+console.log(username);
