43915bb8439a07f78e5ea5699be0efeed08fa09c
Dhruv Jain <114809832+dhruvjain2004@users.noreply.github.com>
2025-02-04 17:37:39 +0530

    Create datatype.js

---
+++ core js/datatype.js
@@ -0,0 +1,19 @@
+fullname = "dhruvjain"
+console.log(fullname);
+typeof(fullname);
+let student = {
+    sname : "rohan",
+    age : 19,
+    cgpa : 9.1,
+    ispass : true
+};
+for (let key in student) {
+    console.log("key", key, "value",student[key]); 
+}
+//q1
+const profile = {
+    username : "dhruvjain",
+    isfollow : false,
+    followers : 200,
+    following : 180
+};
